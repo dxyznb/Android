fc151f02fbcc7348266e38175e9196a717b3ed53
Dongxinyu <92635232+dxyznb@users.noreply.github.com>
2024-04-28 22:02:02 +0800

    Add files via upload

---
+++ main/java/com/example/login_page/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.login_page;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textViewRegister = findViewById(R.id.textViewRegister);
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
